256e53e7ec6ba7bbfc187648e713df066f248d9b
Nick Luker <76579997+nck3l@users.noreply.github.com>
2025-06-26 18:27:42 -0700

    added configuration for pointing devices

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
 setxkbmap -layout us &
-xinput --set-prop "TPPS/2 IBM TrackPoint" 354 0.7 &
-xrandr --output LVDS-1 --gamma 0.9:1.0:1.0 &
-picom -b
+export $(dbus-launch) # ad-hoc dbus management rather than wrapping in a dbus-run-session
+xinput set-prop 11 354 0.6 & # makes trackpoint track faster (higher numbers)
+xinput set-prop 10 314 300, 300 & # makes touchpad scroll slower (higher = slower)
+xinput set-prop 10 316 1, 1 & # makes touchpad scroll vertically and horizontally
+picom -b # this daemonizes the process. Settings are in picom.conf
 dunst -conf ~/.config/dunst/config &
 feh --bg-fill --random ~/Pictures/Wallpapers/* &
