f97667877f91128b102b61069c52bdf855a1b26f
Nick Luker <76579997+nck3l@users.noreply.github.com>
2025-06-26 18:06:05 -0700

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-setxkbmap us &
+setxkbmap -layout us &
 xinput --set-prop "TPPS/2 IBM TrackPoint" 354 0.7 &
-xrandr --output LVDS-1 --mode 1366x768 &
-picom --backend glx &
+xrandr --output LVDS-1 --gamma 0.9:1.0:1.0 &
+picom -b
 dunst -conf ~/.config/dunst/config &
 feh --bg-fill --random ~/Pictures/Wallpapers/* &
